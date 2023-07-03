b07a7ed78f9be927f849490044dbb6c7680d1d2d
lfrendahl <lfrendahl3@gmail.com>
2023-07-03 15:05:40 -0400

    time slot generating function created using date-fns, and conditional rendering of the slots

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,5 +1,7 @@
 import { FC, useEffect, useState } from 'react';
 import ReactCalendar from 'react-calendar'
+//help format the calendar date so the generated time slots can start and end at the appropriate hour
+import {add, format} from "date-fns"
 
 interface indexProps {}
 
@@ -9,25 +11,53 @@ interface DateType {
 }
 
 const index: FC<indexProps> = ({}) => {
+    //Set state of null on opening that can be updated on calendar date click and time slot click
     const [date, setDate] = useState<DateType>({
-        dateChosen: null,
+        justDate: null,
         dateTime: null,
     })
 
+    //Function that will create available time slots after a specific calendar date has been chosen. 
+const getSlots = () => {
+    if(!date.justDate) return
 
+    const { justDate } = date
+    const startingSlot = add(justDate, { hours: 9 })
+    const endingSlot = add(justDate, { hours: 19 })
+    const interval = 45 //time slot length in minutes
 
+    const slots = []
+    for (let i = startingSlot; i <= endingSlot; i = add(i, {minutes: interval})) {
+        slots.push(i)
+    }
 
+    return slots
+}
 
+//Populate the slots variable
+const slots = getSlots()
+
+//if there is no date chosen, display the calendar for selection, but once a date is selected display the time slots that are available.
     return (
     <div className='h-screen flex flex-col justify-center items-center'>
-        {date.dateChosen ? (
-            <div>'You chose a date'</div>
-      ) : (
+        {date.justDate ? (
+            <div className='flex gap-4'>
+                {slots?.map((time, id) => (
+                    <div className='rounded-sm bg-green-600 p2' key={`slot-${id}`}>
+                        <button type='button' 
+                                onClick={() => setDate((prev) => ({...prev, dateTime: time }))}>
+                            {format(time, 'kk:mm')}
+                        </button>
+                    </div>
+                   ))}
+            </div>
+        )
+       : (
         <ReactCalendar 
             minDate={new Date()}
             className='REACT-CALENDAR p-2' 
             view='month'
-            onClickDay={(date) => setDate((prev) => ({...prev, dateChosen: date }))} 
+            onClickDay={(date) => setDate((prev) => ({...prev, justDate: date }))} 
          />
          )}
     </div>
