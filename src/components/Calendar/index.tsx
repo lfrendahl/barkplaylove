76a7ee1fb9d3b5a5c403887358c885045c4dc37f
lfrendahl <lfrendahl3@gmail.com>
2023-07-03 14:22:53 -0400

    created a conditional render of the calendar and imported usestate into the calendar .tsx file.

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,16 +1,37 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import ReactCalendar from 'react-calendar'
 
 interface indexProps {}
 
+interface DateType {
+    justDate: Date | null
+    dateTime: Date | null
+}
+
 const index: FC<indexProps> = ({}) => {
-    return <div><ReactCalendar 
-    minDate={new Date()}
-    className='REACT-CALENDAR p-2' 
-    view='month'
-    onClickDay={(date) => console.log(date)} 
-    />
+    const [date, setDate] = useState<DateType>({
+        dateChosen: null,
+        dateTime: null,
+    })
+
+
+
+
+
+    return (
+    <div className='h-screen flex flex-col justify-center items-center'>
+        {date.dateChosen ? (
+            <div>'You chose a date'</div>
+      ) : (
+        <ReactCalendar 
+            minDate={new Date()}
+            className='REACT-CALENDAR p-2' 
+            view='month'
+            onClickDay={(date) => setDate((prev) => ({...prev, dateChosen: date }))} 
+         />
+         )}
     </div>
+    )
 }
 
 export default index
